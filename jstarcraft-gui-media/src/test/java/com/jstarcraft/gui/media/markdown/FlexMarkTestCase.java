08642da7ea2a6cce9faed029f98f589eb90a0c6a
Birdy <Birdy@LAPTOP-22TTFGMB>
2022-04-16 19:18:09 +0800

    FlexMark单元测试.

--- jstarcraft-gui-media/src/test/java/com/jstarcraft/gui/media/markdown/FlexMarkTestCase.java
+++ jstarcraft-gui-media/src/test/java/com/jstarcraft/gui/media/markdown/FlexMarkTestCase.java
@@ -2,28 +2,45 @@ package com.jstarcraft.gui.media.markdown;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 import org.junit.Test;
 
 import com.jstarcraft.core.utility.StringUtility;
+import com.vladsch.flexmark.ext.tables.TableExtractingVisitor;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.formatter.Formatter;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.DataSet;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+import com.vladsch.flexmark.util.format.MarkdownTable;
+import com.vladsch.flexmark.util.sequence.LineAppendableImpl;
 
 public class FlexMarkTestCase {
 
     @Test
     public void test() throws Exception {
-
         try (InputStream stream = FlexMarkTestCase.class.getResourceAsStream("markdown.md"); InputStreamReader reader = new InputStreamReader(stream, StringUtility.CHARSET)) {
-            Parser parser = Parser.builder().build();
-            Formatter formatter = Formatter.builder().build();
+            // 设置支出表格
+            DataSet option = new MutableDataSet().set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create())).toImmutable();
+            Parser parser = Parser.builder(option).build();
+            Formatter formatter = Formatter.builder(option).build();
 
+            LineAppendableImpl buffer = new LineAppendableImpl(0);
             Node document = parser.parseReader(reader);
-            String commonmark = formatter.render(document);
+            formatter.render(document, buffer);
+            System.out.println(buffer.toString());
 
-            System.out.println("\n\nCommonMark\n");
-            System.out.println(commonmark);
+            TableExtractingVisitor tableVisitor = new TableExtractingVisitor(option);
+            MarkdownTable[] tables = tableVisitor.getTables(document);
+
+            System.out.println(tables.length);
+            for (MarkdownTable table : tables) {
+                buffer = new LineAppendableImpl(0);
+                table.appendTable(buffer);
+                System.out.println(buffer.toString());
+            }
         }
 
     }
